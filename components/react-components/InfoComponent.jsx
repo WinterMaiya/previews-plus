25cea4aeafec1cfb99598c639f1ed2ec382cc4f4
Maiya Winter <wintermaiya@gmail.com>
2022-10-11 10:22:43 -0600

    Updated info page component

--- components/react-components/InfoComponent.jsx
+++ components/react-components/InfoComponent.jsx
@@ -10,30 +10,37 @@ const infoComponent = () => {
 							</h2>
 						</div>
 					</div>
-					<div className="col">
-						<p>
-							This project was created using Next.js, Prisma, PostgreSQL,
-							Next-Auth, Swiper, and more... You can view this project on{" "}
-							<a href="https://github.com/WinterMaiya/previews-plus">Github</a>.
-							I created this project as an attempt to replicate popular
-							streaming services. This was my first project with Next.js so it
-							was an excellent learning experience for me to learn and improve.
-							Any advice or questions are more then welcome, you can contact me
-							through my Github page.
-						</p>
-						<p>
-							External API data thanks to <strong>TMDB:</strong>
-							{/*  */}
-							<div>
-								<a href="https://www.themoviedb.org">
-									<img
-										src="/images/tmdb_logo.svg"
-										width="100"
-										height="100"
-									></img>
+					<div className="row">
+						<div className="col text-center">
+							<p>
+								This project was created using Next.js, Prisma, PostgreSQL,
+								Next-Auth, Swiper, and more... You can view this project on{" "}
+								<a
+									href="https://github.com/WinterMaiya/previews-plus"
+									className="text-white"
+								>
+									Github
 								</a>
-							</div>
-						</p>
+								. I created this project as an attempt to replicate popular
+								streaming services. This was my first project with Next.js so it
+								was an excellent learning experience for me to learn and
+								improve. Any advice or questions are more then welcome, you can
+								contact me through my Github page.
+							</p>
+							<p>
+								External API data thanks to <strong>TMDB:</strong>
+								{/*  */}
+								<div>
+									<a href="https://www.themoviedb.org">
+										<img
+											src="/images/tmdb_logo.svg"
+											width="100"
+											height="100"
+										></img>
+									</a>
+								</div>
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
